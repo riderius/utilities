adfa6f117cfedbcf437f9f187208de4acc9ee0de
RIDERIUS <riderius.yt@gmail.com>
2020-08-29 15:24:37 +0300

    Initial commit
    First, this is the first commit. Secondly, I added the first utility "Re-name.py". This utility quickly renames files in the same style.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Utilities
+
+Hi! **Utilities** is a collection of my utilities in Python 3.8.
+
+## Modules Used:
+
+1. glob2
+
+Python 3.8 or higher is recommended for using this programs. You need to install modules from requirements.txt file. You can do this with the following command:
+
+> pip install -r requirements.txt
+
+**glob2** is a Python module that sorts files by format.
+
+## Utilities
+
+1. Re-Name.py
+
+### Re-Name.py
+
+**Re-Name.py** is a utility to quickly rename photos and videos in name_i.format style.
---
+++ Re-Name.py
@@ -0,0 +1,48 @@
+import os
+import shutil
+import glob2
+
+path = input('Путь: ')
+
+name = input('Имя: ')
+
+i = '1'
+
+os.chdir(path)
+
+files = os.listdir()
+
+for file in glob2.glob("*jpg"):
+    i = str(i)
+    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.jpg')
+    i = int(i)
+    i += 1
+
+for file in glob2.glob("*png"):
+    i = str(i)
+    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.png')
+    i = int(i)
+    i += 1
+
+for file in glob2.glob("*tif"):
+    i = str(i)
+    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.tif')
+    i = int(i)
+    i += 1
+
+for file in glob2.glob("*gif"):
+    i = str(i)
+    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.gif')
+    i = int(i)
+    i += 1
+
+for file in glob2.glob("*heic"):
+    i = str(i)
+    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.heic')
+    i = int(i)
+    i += 1
+
+for file in glob2.glob("*mp4"):
+    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.mp4')
+    i = int(i)
+    i += 1
