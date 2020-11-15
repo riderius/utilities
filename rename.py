a44cc227d77c1962e7ec51e7102ac9efa377baf3
RIDERIUS <riderius.help@gmail.com>
2020-11-15 05:17:23 +0300

    Added documentation for functions

--- rename.py
+++ rename.py
@@ -1,51 +1,43 @@
-""" rename.py is a utility to quickly rename photos
-    and videos in name_i.format style. """
+""" rename.py is a utility to quickly rename photos and videos in name_i.format style. """
 
 import os
 import shutil
 import glob2
 
-path = input('Путь: ')
 
-name = input('Имя: ')
+def rename(file_format: str, path: str, name: str, i: int) -> None:
+    """
 
-i = '1'
+    Function for quickly rename photos and videos in name_i.format style.
 
-os.chdir(path)
+    Args:
+        file_format (str): A variable that shows in which format to rename files.
+        path (str): A variable in which the path to the files that will be renamed is stored.
+        name (str): A variable that stores the name that will come before the file number.
+        i (int): A variable that stores the start number of the file count.
 
-files = os.listdir()
+    Returns:
+        None: The function returns nothing.
 
-for file in glob2.glob("*jpg"):
-    i = str(i)
-    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.jpg')
-    i = int(i)
-    i += 1
+    """
+    for file in glob2.glob(f'*{file_format}'):
+        i = str(i)
+        shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.{file_format}')
+        i = int(i)
+        i += 1
 
-for file in glob2.glob("*png"):
-    i = str(i)
-    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.png')
-    i = int(i)
-    i += 1
 
-for file in glob2.glob("*tif"):
-    i = str(i)
-    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.tif')
-    i = int(i)
-    i += 1
+input_path = input('Path: ')
 
-for file in glob2.glob("*gif"):
-    i = str(i)
-    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.gif')
-    i = int(i)
-    i += 1
+input_name = input('Name: ')
 
-for file in glob2.glob("*heic"):
-    i = str(i)
-    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.heic')
-    i = int(i)
-    i += 1
+input_i = input('Number: ')
 
-for file in glob2.glob("*mp4"):
-    shutil.move(f'{path}\\{file}', f'{path}\\{name}_{i}.mp4')
-    i = int(i)
-    i += 1
+os.chdir(input_path)
+
+rename('jpg', input_path, input_name, input_i)
+rename('png', input_path, input_name, input_i)
+rename('tif', input_path, input_name, input_i)
+rename('gif', input_path, input_name, input_i)
+rename('heic', input_path, input_name, input_i)
+rename('mp4', input_path, input_name, input_i)
--- text2binary.py
+++ text2binary.py
@@ -1,18 +1,37 @@
-""" Module for translate text to binary code and
-    for translate binary code to text.
-        Functions are taken from here:
-            http://bit.do/functions_from_text2binary """
+""" Module for translate text to binary code and for translate binary code to text.
+    Functions are taken from here:
+        http://bit.do/functions_from_text2binary """
 
 
-def text_to_bits(text: str) -> str:
-    """ Function for translate text to binary code. """
+def text_to_binary(text: str) -> None:
+    """
+
+    Function for translate text to binary code.
+
+    Args:
+        text (str): A variable that stores text that will be converted to binary code.
+
+    Returns:
+        None: The function returns nothing.
+
+    """
 
     text = bin(int.from_bytes(text.encode('utf-8'), 'big'))[2:]
     print(text.zfill(8 * ((len(text) + 7) // 8)))
 
 
-def text_from_bits(text: str) -> str:
-    """ Function for translate binary code to text. """
+def text_from_binary(text: str) -> None:
+    """
+
+    Function for translate binary code to text.
+
+    Args:
+        text (str): A variable that stores binary code that will be converted to normal text.
+
+    Returns:
+        None: The function returns nothing.
+
+    """
 
     text = int(text, 2)
     print(text.to_bytes((text.bit_length() + 7) //
@@ -22,9 +41,9 @@ def text_from_bits(text: str) -> str:
 function = int(input('1)Text to Binary Code \n2)Binary Code to Text \n'))
 if function == 1:
     input_text = input('Enter text:\n')
-    text_to_bits(input_text)
+    text_to_binary(input_text)
 elif function == 2:
     input_text = input('Enter text:\n')
-    text_from_bits(input_text)
+    text_from_binary(input_text)
 else:
     print('This function is NOT')
